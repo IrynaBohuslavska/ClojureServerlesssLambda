d63779d5ed58aa1e4555ea86655212104d261de6
IrynaBohuslavska <61269582+IrynaBohuslavska@users.noreply.github.com>
2021-07-22 17:55:44 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
-curl -H 'Content-Type: application/json' --data '{"expr": "60 + 5"}' https://wl6sv63rcd.execute-api.us-east-2.amazonaws.com/calc
+URL="https://wl6sv63rcd.execute-api.us-east-2.amazonaws.com/dev/calc"
+
+
+curl -XPOST $URL\
+	 -H "Content-Type: application/json"\
+	 --data '{"expr": "5 + 5"}'
